93ed13ec7790b754dc53282e8567fea71e393d66
0long0 <1392145826@qq.com>
2022-01-06 23:23:19 +0800

    git commit

--- app/src/main/java/com/example/loginactivity/MainActivity.java
+++ app/src/main/java/com/example/loginactivity/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.loginactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
+
+        //注册页面跳转
+        Button button_register = findViewById(R.id.button_register);
+        button_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_register = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent_register);
+            }
+        });
 
     }
 }
